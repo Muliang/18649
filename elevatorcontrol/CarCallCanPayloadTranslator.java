cc060df117ac20acfdc14ec0ea8f69548ec49a8d
Wenhui Hu <wenhui@Wenhuis-MacBook-Pro.local>
2013-10-11 20:56:03 -0400

    add header

---
+++ elevatorcontrol/CarCallCanPayloadTranslator.java
@@ -0,0 +1,32 @@
+package simulator.elevatorcontrol;
+
+import simulator.framework.Hallway;
+import simulator.framework.ReplicationComputer;
+import simulator.payloads.CanMailbox.ReadableCanMailbox;
+import simulator.payloads.CanMailbox.WriteableCanMailbox;
+
+
+public class CarCallCanPayloadTranslator extends BooleanCanTranslator {
+
+	
+    /**
+     * CAN translator for messages from CarCall sensors
+     * @param payload CAN payload object whose message is interpreted by this translator
+     * @param floor replication index
+     * @param hallway replication index
+     */
+	public CarCallCanPayloadTranslator(WriteableCanMailbox payload, int floor, Hallway hallway) {
+		super(payload, MessageDictionary.CAR_CALL_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway), "CarCall" + ReplicationComputer.computeReplicationId(floor, hallway));
+	}
+
+    /**
+     * CAN translator for messages from CarCall sensors
+     * @param payload CAN payload object whose message is interpreted by this translator
+     * @param floor replication index
+     * @param hallway replication index
+     */
+	public CarCallCanPayloadTranslator(ReadableCanMailbox payload, int floor, Hallway hallway) {
+		super(payload, MessageDictionary.CAR_CALL_BASE_CAN_ID + ReplicationComputer.computeReplicationId(floor, hallway), "CarCall" + ReplicationComputer.computeReplicationId(floor, hallway));
+	}
+
+}
