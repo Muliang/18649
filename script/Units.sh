f4b4e7ac1fb9cd1d627c7fad184186995a5f4019
Wenhui Hu <wenhui@Wenhuis-MacBook-Pro.local>
2013-12-02 16:50:47 -0500

    updated script for unit test

--- script/Units.sh
+++ script/Units.sh
@@ -7,11 +7,12 @@ rm -f *.stats
 
 java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/HallButtonControl.cf -mf unit_test/HallButtonControl.mf | grep "Message"
 java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/CarButtonControl.cf -mf unit_test/CarButtonControl.mf| grep "Message"
-java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/drivecontrol.cf -mf unit_test/drivecontrol.mf | grep "Message"
+java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/DriveControl.cf -mf unit_test/DriveControl.mf | grep "Message"
+java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/DoorControl.cf -mf unit_test/DoorControl.mf | grep "Message"
 java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/CarPositionControl.cf -mf unit_test/CarPositionControl.mf | grep "Message"
 java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/LanternControl.cf -mf unit_test/LanternControl.mf | grep "Message"
 java simulator/framework/Elevator -rt 0s -head HEADER -cf unit_test/Dispatcher.cf -mf unit_test/Dispatcher.mf | grep "Message"
 
 grep "Failed" *.stats
 
-rm -f *.stats
+#rm -f *.stats
